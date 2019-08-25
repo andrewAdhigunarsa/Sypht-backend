dbeaab59ac3cafa0e71d80b70561e716f3af660e
Andrew Adhigunarsa <adhigunarsa@192-168-1-106.tpgi.com.au>
2019-08-26 02:53:20 +1000

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const multer = require('multer');
+const cors = require('cors');
 
 app.use(morgan('combined'));
+app.use(cors());
 
 app.get("/",(req,res)=>{
     console.log('responding to request test');
