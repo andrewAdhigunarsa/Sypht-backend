9780d4489dbd6ed4f6e12964c0850d64107e894e
Andrew Adhigunarsa <adhigunarsa@192-168-1-106.tpgi.com.au>
2019-08-27 08:09:06 +1000

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const axios = require('axios');
 const multer = require('multer');
 const cors = require('cors');
 
@@ -9,7 +11,39 @@ app.use(cors());
 
 app.get("/",(req,res)=>{
     console.log('responding to request test');
-    res.send('Woohoo');
+    res.send({hello:'testing'});
+})
+
+app.post("/token", (req,res) => {
+
+    const URL = 'https://login.sypht.com/oauth/token/';
+    const body = {
+        client_id: process.env.NODE_APP_CLIENT_ID,
+        client_secret: process.env.NODE_APP_CLIENT_SECRET,
+        audience: "https://api.sypht.com",
+        grant_type: "client_credentials"
+    }
+    const options = {
+        method: "POST",
+        url: URL,
+        data: JSON.stringify(body),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    };
+    axios(options).then(
+        (response)=>{
+            console.log(response.data);
+            res.send(response.data);
+        }
+    ).catch(
+        (e)=>{
+            console.error(e)
+            res.send(e)
+        }
+    )
+    
 })
 
 app.listen(3003,()=>{
